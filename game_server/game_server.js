47bcce8cf4e5ffc850f0629e27bc2a1588c7d1a8
KUROKI Shinsuke <s-kuroki@aiming-inc.com>
2018-01-06 20:47:10 +0900

    Add character to stage

--- game_server/game_server.js
+++ game_server/game_server.js
@@ -3,15 +3,18 @@ function handler (req, res) {
   res.end('hello');
 }
 
-var app = require('http').createServer(handler)
+var app = require('http').createServer(handler);
 var io = require('socket.io')(app);
-var fs = require('fs');
+var characters = [];
 
 app.listen(8080);
 
 io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data) {
     console.log(data);
   });
+
+  var character = { x: 320, y: 240 };
+  characters.push(character);
+  socket.emit('character', character);
 });
--- pages/kick_it_down.js
+++ pages/kick_it_down.js
@@ -4,10 +4,16 @@ function init() {
   background.graphics.beginFill("Black").drawRect(0, 0, 640, 480);
   stage.addChild(background);
   stage.update();
-}
 
-var socket = io('http://localhost:8080');
-socket.on('news', function (data) {
-  console.log(data);
-  socket.emit('my other event', { my: 'data' });
-});
+  var socket = io('http://localhost:8080');
+
+  socket.on('character', function (data) {
+    console.log(data);
+    var character = new createjs.Shape();
+    character.graphics.beginFill("Red").drawRect(-16, -16, 16, 16);
+    character.x = data.x;
+    character.y = data.y;
+    stage.addChild(character);
+    stage.update();
+  });
+}
