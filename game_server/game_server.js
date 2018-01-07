d643c0c8fc4281b9bc97e2b768c64abfdaabf05b
KUROKI Shinsuke <s-kuroki@aiming-inc.com>
2018-01-07 14:43:17 +0900

    Make game server code more readable

--- game_server/game_server.js
+++ game_server/game_server.js
@@ -3,20 +3,20 @@ function handler (req, res) {
   res.end('hello');
 }
 
-var app = require('http').createServer(handler);
-var io = require('socket.io')(app);
-var characters = [];
+let app = require('http').createServer(handler);
+let io = require('socket.io')(app);
+let characters = [];
 
 app.listen(8080);
 
-io.on('connection', function (socket) {
-  socket.on('my other event', function (data) {
+io.on('connection', socket => {
+  socket.on('my other event', data => {
     console.log(data);
   });
 
   socket.emit('clock', Date.now());
 
-  var character = { x: 320, y: 240, vx: 32.0, vy: 0.0, from: Date.now() };
+  let character = { x: 320, y: 240, vx: 32.0, vy: 0.0, from: Date.now() };
   characters.push(character);
   socket.emit('character', character);
 });
