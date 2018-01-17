675a8778caad0c1acef9ded3aa0df81181b23b13
KUROKI Shinsuke <s-kuroki@aiming-inc.com>
2018-01-18 01:58:54 +0900

    Add register endpoint to matching server

--- matching_server/matching_server.js
+++ matching_server/matching_server.js
@@ -1,10 +1,22 @@
 let express = require('express');
 let app = express();
+let bodyParser = require('body-parser');
+
+let gameServers = {};
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/match', function (req, res) {
   res.send('game-server-0');
 });
 
+app.post('/register', function (req, res) {
+  console.log('register ' + JSON.stringify(req.body));
+  gameServers[req.body.name] = req.body.state;
+  console.log('gameServers ' + JSON.stringify(gameServers));
+  res.send('ok');
+});
+
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('matching server running');
 });
