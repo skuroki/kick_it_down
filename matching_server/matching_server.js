4708033f2d577d22c036d97ed606d6d9ab67e910
KUROKI Shinsuke <s-kuroki@aiming-inc.com>
2018-01-19 00:13:05 +0900

    Implement matching logic

--- matching_server/matching_server.js
+++ matching_server/matching_server.js
@@ -7,7 +7,24 @@ let gameServers = {};
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/match', function (req, res) {
-  res.send('game-server-0');
+  console.log('match gameServers ' + JSON.stringify(gameServers));
+  let waitings = Object.entries(gameServers).filter((e) => e[1] == 'waiting');
+  console.log('waitings ' + JSON.stringify(waitings));
+  if (waitings.length > 0) {
+    let selected = waitings[Math.floor(Math.random() * waitings.length)];
+    console.log('selected ' + JSON.stringify(selected));
+    res.send(selected[0]);
+    return;
+  }
+  let readies = Object.entries(gameServers).filter((e) => e[1] == 'ready');
+  console.log('readies ' + JSON.stringify(readies));
+  if (readies.length > 0) {
+    let selected = readies[Math.floor(Math.random() * readies.length)];
+    console.log('selected ' + JSON.stringify(selected));
+    res.send(selected[0]);
+    return;
+  }
+  res.send('fullhouse');
 });
 
 app.post('/register', function (req, res) {
